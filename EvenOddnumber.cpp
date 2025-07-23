11fa6041d71f77591c6d0db2a641ba9d54c79539
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-07-23 14:55:26 +0530

    Create EvenOddnumber.cpp

---
+++ EvenOddnumber.cpp
@@ -0,0 +1,23 @@
+// Name : Dhakshayini Usha R
+//PRN:24070123039 ENTC A2
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<=10;i++){
+        if(i%2 == 0){
+        cout<<i<<endl;
+        }
+        else{
+            continue;
+        }
+    }
+    return 0;
+}
+/*Sample Output: 
+0
+2
+4
+6
+8
+10*/
