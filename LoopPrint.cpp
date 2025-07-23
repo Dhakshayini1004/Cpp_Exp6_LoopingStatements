db94e927b1d7b5daa37929566f45c4444e78fb8a
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-07-23 14:57:12 +0530

    Create LoopPrint.cpp

---
+++ LoopPrint.cpp
@@ -0,0 +1,18 @@
+// Name : Dhakshayini Usha R
+//PRN:24070123039 ENTC A2
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=5;i++){
+        cout<<"SIT"<<endl;
+    }
+    return 0;
+}
+/*Sample Output: 
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
