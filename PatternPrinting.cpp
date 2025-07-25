0611dfe89d60ebea436ec58fccfcfcc8d282c315
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-07-25 11:29:41 +0530

    Update PatternPrinting.cpp

--- PatternPrinting.cpp
+++ PatternPrinting.cpp
@@ -35,8 +35,17 @@ cout<<endl;
         cout << endl;
  }   
  cout<<endl;
+ //Simple Eqilateral Triangle
+ for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= row - i; j++){ 
+            cout << " ";}
+        for (int j = 1; j <= i; j++){ 
+            cout << "* ";}
+        cout << endl;
+ }   
+ cout<<endl;
  
- // Equilateral Triangle
+ // Complex Equilateral Triangle
  for(int i = 0; i< row; i++){
      for(int j = 0; j< row - i; j++){
          cout<<" ";
@@ -81,6 +90,12 @@ Enter the number of rows :
  ****
 *****
 
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+
      *
     ***
    *****
@@ -91,4 +106,4 @@ Enter the number of rows :
 2	3	
 4	5	6	
 7	8	9	10	
-11	12	13	14	15*/
+11	12	13	14	15	*/
